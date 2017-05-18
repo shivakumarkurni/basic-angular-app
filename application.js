fc51282d6d3d65f0e5791e32d13152b2350e151f
Narayana <narayana.chinna22@gmail.com>
2017-05-18 15:21:39 +0530

    Navigation Files

---
+++ application.js
@@ -0,0 +1,29 @@
+var app=angular.module("myApp",['ngRoute']);
+app.config(["$locationProvider",function($locationProvider){
+  $locationProvider.hashPrefix('')
+}])
+app.config(["$routeProvider",function($routeProvider){
+ $routeProvider
+		 .when("/",{
+		 	templateUrl:"pages_application/Home.html",
+		 	controller:"myHomeController"
+		 })
+		 .when("/About_us",{
+
+		 	templateUrl:"pages_application/Login.html",
+		 	controller:"myLoginController"
+		 })
+		 .when("/Login",{
+		 	templateUrl:"pages_application/Register.html",
+		 	controller:"myRegisterController"
+		 })
+}])
+app.controller("myHomeController",["$scope",function($scope){
+	$scope.message="This is Home Page";
+}])
+app.controller("myLoginController",["$scope",function($scope){
+  $scope.message="This is Login page";
+}])
+app.controller("myAboutusController",["$scope",function($scope){
+	$scope.message="This is About_us Page";
+}])
