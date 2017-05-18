50ff6a78276bc58ddd0cc2eaa3d9fb333da9c9b5
Narayana <narayana.chinna22@gmail.com>
2017-05-18 11:15:47 +0530

    routing related files

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-var app=angular.module("myApp",[])
+var app=angular.module("myApp",["ngRoute"]);
 
-app.controller("myController",["$scope",function($scope){
-      $scope.message="HelloWorld";
-}]);
+app.config(["$locationProvider","$routeProvider",function($locationProvider,$routeProvider){
+ $locationProvider.hashPrefix('')
+$routeProvider
+  .when("/Home",{
+
+  	templateUrl:"pages/Home.html",
+  	controller:"myHomeController"
+  })
+   .when("/Register",{
+      templateUrl:"pages/Register.html",
+      controller:"myRegisterController"
+   })
+   .when("/Login",{
+      templateUrl:"pages/Login.html",
+      controller:"myLoginController"
+   })
+app.controller("myHomeController",["$scope",function($scope){
+	$scope.message="This is Angular Home Page";
+}])
+app.controller("myRegisterController",["$scope",function($scope){
+	$scope.message="This is Angular  Register Page";
+}])
+app.controller("myLoginController",["$scope",function($scope){
+	$scope.message="This is Angular Login Page";
+}])
+ console.log("this is config");
+}])
